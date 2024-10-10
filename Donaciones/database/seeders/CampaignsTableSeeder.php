bc6438123d82c5e15101ef584f7f90332b0c45a2
melaniquininires <you@example.com>
2024-10-10 13:38:05 -0300

    cambie el campaignfactory porque daba error

--- Donaciones/database/seeders/CampaignsTableSeeder.php
+++ Donaciones/database/seeders/CampaignsTableSeeder.php
@@ -11,8 +11,14 @@ class CampaignsTableSeeder extends Seeder
     public function run()
     {
         $users = User::all();
+
+        if ($users->isEmpty()) {
+            // Si no hay usuarios, creamos algunos
+            $users = User::factory(10)->create();
+        }
+
         Campaign::factory()->count(5)->create([
-            'user_id' => $users->random()->id,
+            'user_id' => $users->random()->id,  // Asignar a un usuario aleatorio
         ]); 
     }
 }
