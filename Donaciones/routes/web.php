afe829484684d96efcfc8567a5c3df64075baab7
AbigailCo <abigail.candelaria.corrales@gmail.com>
2024-11-22 12:09:58 -0300

    TerminadaEstadisticas

--- Donaciones/routes/web.php
+++ Donaciones/routes/web.php
@@ -19,8 +19,9 @@ use Inertia\Inertia;
 
 
 
-
-//EMAIL de notificaciones
+////////////////////////////////////////////////////////////////////////
+//                       NOTIFICACIONES POR MAIL                     //
+////////////////////////////////////////////////////////////////////////
 Route::get('/send-test-mail', function () {
     Mail::to('darvuelvedonaciones@gmail.com')->send(new TestMail());
     return 'Correo de prueba enviado!';
@@ -28,22 +29,39 @@ Route::get('/send-test-mail', function () {
 Route::post('/campaigns/{id}/update-event', function ($id) {
     $campaign = Campaign::findOrFail($id); // Asegúrate de manejar el caso donde no se encuentre la campaña
     event(new CampaignUpdated($campaign));
-
     Mail::to('darvuelvedonaciones@gmail.com')->send(new CampaignUpdateNotification($campaign));
-
     return response()->json(['message' => 'Evento de campaña actualizado y correo enviado.']);
 });
 
+////////////////////////////////////////////////////////////////////////
+//                       INICIAR SESSION CON GOOGLE                   //
+////////////////////////////////////////////////////////////////////////
 
-//Session con google
 Route::get('auth/google', [SocialAuthController::class, 'redirectToGoogle']);
 Route::get('auth/google/callback', [SocialAuthController::class, 'handleGoogleCallback']);
-//API mercado pago
+
+////////////////////////////////////////////////////////////////////////
+//                       BREEZE INICIO DE SESSION                    //
+////////////////////////////////////////////////////////////////////////
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+////////////////////////////////////////////////////////////////////////
+//                       INTEGRACION DE MERCADO PAGO                  //
+////////////////////////////////////////////////////////////////////////
 
 Route::get('/donations/success', [PaymentController::class, 'success'])->name('donations.success');
 Route::get('/donations/failure', [PaymentController::class, 'failure'])->name('donations.failure');
 Route::get('/donations/pending', [PaymentController::class, 'pending'])->name('donations.pending');
-//WELCOME Y DESHBOARD
+
+////////////////////////////////////////////////////////////////////////
+//                       RUTAS DE PAGINAS INTERNA                     //
+////////////////////////////////////////////////////////////////////////
+
+//WELCOME
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
@@ -52,62 +70,77 @@ Route::get('/', function () {
         'phpVersion' => PHP_VERSION,
     ]);
 });
+
+//DASHBOAR o panel de usuario
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-//PAGES
+//CAMPAIGNS todas las campañas
 Route::get('/campaign', function () {
     return Inertia::render('Campaign/Campaign'); // Asegúrate de usar el path correcto
 })->middleware(['auth', 'verified'])->name('campaign');
-Route::get('/campaigns', [CampaignController::class, 'index']);
+
+//CREATE CAMPAIGN 
 Route::get('/CreateCampaign', function () {
     return Inertia::render('Campaign/CreateCampaign'); // Asegúrate de usar el path correcto
 })->middleware(['auth', 'verified'])->name('CreateCampaign');
-Route::get('/campaigns/search', [CampaignController::class, 'search']);
-Route::get('/categories', [CategoryController::class, 'index']);
-//Seccion de estadisticass globales
+
+////////////////////////////////////////////////////////////////////////
+//                       ESTADISTICAS GLOBALES                        //
+////////////////////////////////////////////////////////////////////////
+
 Route::get('/estadisGenerales', [CampaignController::class, 'estadisGenerales']);
 Route::get('/UserDona', [CampaignController::class, 'donationCorrelation']);
 Route::get('/estadisConvinada', [CampaignController::class, 'getMetrics']);
+Route::get('/allCampaigns', [CampaignController::class, 'exportAllCampaigns']);
 Route::get('/EstadisticasGlobales', function () {
     return Inertia::render('Campaign/CampaignGeneral'); // Asegúrate de usar el path correcto
 })->middleware(['auth', 'verified'])->name('EstadisticasGlobales');
 
-//CONTROLADORES
+////////////////////////////////////////////////////////////////////////
+//                       CONTROLADORES                                //
+////////////////////////////////////////////////////////////////////////
+
+//COUNT para las card del panel
 Route::get('/campaign-count', [CampaignController::class, 'count'])->name('campaign.count');
 Route::get('/user-campaigns/count', [CampaignController::class, 'countUserCampaigns'])->middleware('auth');
 Route::get('/user-favorites/count', [CampaignController::class, 'countUserFavorites'])->middleware('auth');
+//BUSCADOR
+Route::get('/campaigns/search', [CampaignController::class, 'search']);
+//CATEGORIAS
+Route::get('/categories', [CategoryController::class, 'index']);
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-Route::middleware('auth:sanctum')->group(function() {
+////////////////////////////////////////////////////////////////////////
+//                       CONTROLADORES de campañas                    //
+////////////////////////////////////////////////////////////////////////
+Route::middleware('auth:sanctum')->group(function () {
     Route::get('/campaigns', [CampaignController::class, 'index']);
     Route::post('/api/campaigns', [CampaignController::class, 'store'])->name('campaign.store');
     Route::get('/my-campaigns', [CampaignController::class, 'myCampaigns'])->name('myCampaigns');
     // Para mostrar solo las campañas del usuario
-Route::get('/my-campaigns/{id}', function($id) {
-    return app(CampaignController::class)->show($id, true);
-})->name('myCampaigns.show');
-//Para mostrar todas las campañas
+    Route::get('/my-campaigns/{id}', function ($id) {
+        return app(CampaignController::class)->show($id, true);
+    })->name('myCampaigns.show');
+    //Para mostrar todas las campañas
     Route::get('/campaigns/{id}', [CampaignController::class, 'show'])->name('campaigns.show');
-  
     Route::post('/campaigns/{id}/payment-preference', [CampaignController::class, 'createPaymentPreference']);
-
-    /* Route::get('/campaigns/{id}', [CampaignController::class, 'show']); */
-
-
-
     Route::put('/campaigns/{id}', [CampaignController::class, 'update']);
-   // Route::get('/campaigns/{id}/edit', [CampaignController::class, 'edit'])->name('campaign.edit');
-
     Route::delete('/campaigns/{id}', [CampaignController::class, 'destroy']);
 });
+Route::get('/campaigns', [CampaignController::class, 'index']);
+Route::get('/edit-campaign/{id}', function ($id) {
+    return Inertia::render('Campaign/EditCampaign', ['id' => $id]);
+})->middleware(['auth', 'verified'])->name('editCampaign');
+Route::get('/campaigns/{id}/edit', function ($id) {
+    $campaign = Campaign::findOrFail($id); // Asegúrate de importar el modelo Campaign
+    return response()->json($campaign);
+})->middleware(['auth', 'verified'])->name('campaign.edit');
 
-//RUTAS DE FAVORITOS
+
+////////////////////////////////////////////////////////////////////////
+//                       CONTROLADORES de favoritos                   //
+////////////////////////////////////////////////////////////////////////
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/favorites/{id}', [FavoriteController::class, 'store']); // Para agregar a favoritos
@@ -116,24 +149,15 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/favorites/{id}', [FavoriteController::class, 'show']); // Para verificar si es favorito
 });
 Route::middleware('auth:sanctum')->get('/favorites', [FavoriteController::class, 'index'])->name('favoritos');
-
-Route::get('/edit-campaign/{id}', function ($id) {
-    return Inertia::render('Campaign/EditCampaign', ['id' => $id]);
-})->middleware(['auth', 'verified'])->name('editCampaign');
-
-Route::get('/campaigns/{id}/edit', function ($id) {
-    $campaign = Campaign::findOrFail($id); // Asegúrate de importar el modelo Campaign
-    return response()->json($campaign);
-})->middleware(['auth', 'verified'])->name('campaign.edit');
-
-
+////////////////////////////////////////////////////////////////////////
+//                       CONTROLADORES de donaciones                 //
+////////////////////////////////////////////////////////////////////////
 Route::post('/donations', [DonationController::class, 'store']);
 Route::get('/campaigns/{id}/donations', [CampaignController::class, 'getDonations']);
-/*
-Route::middleware('auth')->group(function () {
-    Route::post('/comments', [CommentController::class, 'store']);
-    Route::get('/campaigns/{campaign_id}/comments', [CommentController::class, 'index']);
-}); */
+
+////////////////////////////////////////////////////////////////////////
+//                       CONTROLADORES de comentarios                 //
+////////////////////////////////////////////////////////////////////////
 
 Route::get('/campaigns/{campaign}/comments', [CommentController::class, 'index']);
 Route::post('/comments', [CommentController::class, 'store']);
@@ -141,8 +165,10 @@ Route::post('/campaigns/{id}/notes', [CampaignController::class, 'addNote']);
 Route::get('/campaigns/{campaign_id}/notes', [CampaignController::class, 'getNotes']);
 
 
-//prueba de ruta
+////////////////////////////////////////////////////////////////////////
+//                            PRUEBA                                  //
+////////////////////////////////////////////////////////////////////////
 Route::get('/test', function () {
     return response()->json(['message' => '¡Ruta funcionando!']);
 });
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
